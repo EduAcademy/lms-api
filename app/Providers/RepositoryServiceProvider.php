55a5045ea9f1cb5edb5eeb3f5fa1becbd2a30a79
OND10 <osamadammag84@gmail.com>
2024-12-18 02:24:25 +0300

    [solved] students

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,10 +7,12 @@ use App\Contracts\GenericRepositoryInterface;
 use App\Repositories\DepartmentRepository;
 use App\Contracts\DepartmentRepositoryInterface;
 use App\Contracts\InstructorRepositoryInterface;
+use App\Contracts\StudentRepositoryInterface;
 use App\Repositories\InstructorRepository;
 use App\Contracts\UserRepositoryInterface;
 use App\Repositories\CourseRepository;
 use App\Repositories\GenericRepository;
+use App\Repositories\StudentRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,6 +29,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(InstructorRepositoryInterface::class, InstructorRepository::class);
         $this->app->bind(GenericRepositoryInterface::class, GenericRepository::class);
         $this->app->bind(CourseRepositoryInterface::class, CourseRepository::class);
+        $this->app->bind(StudentRepositoryInterface::class, StudentRepository::class);
     }
 
     /**
--- app/Providers/ServicesServiceProvider.php
+++ app/Providers/ServicesServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use App\Interfaces\Services\CourseServiceInterface;
 use App\Services\DepartmentService;
 use App\Interfaces\Services\DepartmentServiceInterface;
+use App\Interfaces\Services\StudentServiceInterface;
 use App\Interfaces\Services\UserServiceInterface;
 use App\Services\CourseService;
+use App\Services\StudentService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +23,7 @@ class ServicesServiceProvider extends ServiceProvider
         $this->app->bind(UserServiceInterface::class, UserService::class);
         $this->app->bind(DepartmentServiceInterface::class, DepartmentService::class);
         $this->app->bind(CourseServiceInterface::class, CourseService::class);
+        $this->app->bind(StudentServiceInterface::class, StudentService::class);
     }
 
     /**
