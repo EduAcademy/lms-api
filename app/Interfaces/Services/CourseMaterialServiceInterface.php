64ed2fbd78838452f6eb93b7412fcb29743c21a7
OND10 <osamadammag84@gmail.com>
2024-12-26 02:16:09 +0300

    [feat] add course material repos crud, services not all methods still controller

---
+++ app/Interfaces/Services/CourseMaterialServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Contracts\Interfaces\Services;
+
+interface CourseMaterialServiceInterface
+{
+    //
+    public function getAllCourseMaterials();
+    public function getCourseMaterialById($id);
+    public function getCourseMaterialByCourseId($courseId);
+    public function getCourseMaterialByInstructorId($instructorId);
+    public function createCourseMaterial(array $data);
+    public function updateCourseMaterial($id, array $data);
+    public function deleteCourseMaterial($id);
+}
---
+++ app/Services/CourseMaterialService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\CourseMaterialRepositoryInterface;
+use App\Contracts\Interfaces\Services\CourseMaterialServiceInterface;
+use App\Models\CourseMaterial;
+use App\Repositories\GenericRepository;
+use App\Shared\Constants\StatusResponse;
+use App\Shared\Handler\Result;
+use Illuminate\Support\Facades\Validator;
+
+class CourseMaterialService implements CourseMaterialServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    private $courseMaterialRepository;
+    private $genericRepository;
+    public function __construct(CourseMaterialRepositoryInterface $courseMaterialRepository)
+    {
+        //
+        $this->courseMaterialRepository = $courseMaterialRepository;
+        $this->genericRepository = new GenericRepository(new CourseMaterial);
+    }
+
+
+    public function getAllCourseMaterials()
+    {
+        $result = $this->courseMaterialRepository->getAll();
+
+        return Result::success($result, 'Get All Materials Successfully', StatusResponse::HTTP_OK);
+    }
+
+    public function getCourseMaterialById($id)
+    {
+        $result = $this->courseMaterialRepository->getById($id);
+
+        return Result::success($result, 'Found the Course Material Successfully', StatusResponse::HTTP_OK);
+    }
+
+    public function getCourseMaterialByCourseId($courseId)
+    {
+        $result = $this->courseMaterialRepository->getByCourseId($courseId);
+
+        return Result::success($result, 'Found the Course Material Successfully by Course', StatusResponse::HTTP_OK);
+    }
+
+    public function getCourseMaterialByInstructorId($instructorId)
+    {
+        $result = $this->courseMaterialRepository->getByInstructorId($instructorId);
+
+        return Result::success($result, 'Found the Course Material Successfully by Instructor', StatusResponse::HTTP_OK);
+    }
+
+    public function createCourseMaterial(array $data)
+    {
+        $validator = Validator::make($data, [
+            'name' => 'required|string',
+            'type' => 'required|in:theoretical,practical',
+            'url' => 'nullable|url',
+            'course_id' => 'required|integer|exists:courses,id',
+            'instructor_id' => 'required|integer|exists:instructors,id'
+        ]);
+
+        if ($validator->fails()) {
+            return Result::error('Validation failed', 422, $validator->errors());
+        }
+        $result = $this->courseMaterialRepository->create($data);
+
+        return Result::success($result, 'Course Material is Created Successfully', StatusResponse::HTTP_CREATED);
+    }
+
+    public function updateCourseMaterial($id, array $data)
+    {
+
+    }
+
+    public function deleteCourseMaterial($id)
+    {
+
+    }
+
+
+}
